59dd727a6d85dd6b68a1f6b967bed46f5171b435
Rahul <dasr53357@gmail.com>
2024-11-02 13:11:51 +0530

    fifth changes

--- src/Component/NavBar/Nav.jsx
+++ src/Component/NavBar/Nav.jsx
@@ -1,25 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Nav.css';
-function Nav() 
-{
+
+function Nav() {
   return (
     <>
+      <div className="nav-container">
         <div className="nav flex">
-            <div className="part"><img src="https://img.weddingbazaar.com/shaadisaga_production/static/vendor_categories/WeddingBazaarLogo.svg" alt="" /></div>
-            <button className="part flex"><input type="text" placeholder='Search for event...'/><i className="fa-solid fa-magnifying-glass"></i></button>
-            <div className="part">
-                <ul className='flex'>
-                    <Link to="/Home"><li>Home</li></Link>
-                    <Link to="/login"><li>Login</li></Link>
-                    <Link to="/blog"><li>Blog</li></Link>
-                    {/* <Link to="/ysff"><li>Contacts</li></Link> */}
-                    <Link to="/vendor"><li>Are You Vendor?..</li></Link>
-                </ul>
-            </div>
+          <div className="logo-part">
+            <img
+              src="https://img.weddingbazaar.com/shaadisaga_production/static/vendor_categories/WeddingBazaarLogo.svg"
+              alt="Logo"
+            />
+          </div>
+          <div className="search-part">
+            <button className="search-btn flex">
+              <input type="text" placeholder="Search for event..." />
+              <i className="fa-solid fa-magnifying-glass"></i>
+            </button>
+          </div>
+          <div className="nav-links-part">
+            <ul className="nav-links flex">
+              <Link to="/Home">
+                <li>Home</li>
+              </Link>
+              <Link to="/login">
+                <li>Login</li>
+              </Link>
+              <Link to="/blog">
+                <li>Blog</li>
+              </Link>
+              <Link to="/vendor">
+                <li>Are You Vendor?..</li>
+              </Link>
+            </ul>
+          </div>
         </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
